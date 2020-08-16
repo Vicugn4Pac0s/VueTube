ccc46e25574237aee9809f5d88048ffba234f599
YOSHIKI <vicugnapacos.g@gmail.com>
2020-08-16 20:23:39 +0900

    fix SearchForm

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="searchForm">
-    <input size="40" :value="keyword" @input="$emit('inputted', $event)" placeholder="検索キーワードを入力" />
-    <button @click="$emit('search', $event)"><slot /></button>
+    <input
+      size="40"
+      class="searchForm_input"
+      :value="keyword"
+      @input="$emit('inputted', $event)"
+      placeholder="検索キーワードを入力"
+    />
+    <button class="searchForm_btn" @click="$emit('search', $event)">
+      <slot />
+    </button>
   </div>
 </template>
 
@@ -9,7 +17,7 @@
 export default {
   name: "SearchForm",
   props: {
-    keyword: String
+    keyword: String,
   },
 };
 </script>
@@ -17,6 +25,33 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .searchForm {
+  .searchForm_input {
+    font: 15px/24px sans-serif;
+    box-sizing: border-box;
+    width: 300px;
+    padding: 0.3em;
+    transition: 0.3s;
+    letter-spacing: 1px;
+    color: #aaaaaa;
+    border: none;
+    border-bottom: 2px solid #1b2538;
+    background: transparent;
+    margin-right: 0.5em;
+  }
+  .searchForm_input:focus {
+    border-bottom: 2px solid #da3c41;
+    outline: none;
+  }
+  .searchForm_btn {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    outline: none;
+    appearance: none;
 
+    background: #f9f9f9;
+    border: 2px solid #eee;
+    padding: 0.3em 1em;
+  }
 }
 </style>
