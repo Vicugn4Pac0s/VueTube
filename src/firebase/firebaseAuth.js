006ee706f5440d644501969b3d74ce7611a560de
Yoshiki Yamada <vicugnapacos.g@gmail.com>
2021-01-18 14:34:54 +0900

    getUser追加

--- src/firebase/firebaseAuth.js
+++ src/firebase/firebaseAuth.js
@@ -3,6 +3,7 @@ import firebase from "@/firebase/firebase";
 class firebaseAuth {
   constructor() {
     this.auth = firebase.auth();
+    this.user = null;
     this.state = 0;
     this.events();
   }
@@ -10,14 +11,19 @@ class firebaseAuth {
     let self = this;
     self.auth.onAuthStateChanged((user) => {
       if (user) {
+        self.user = user;
         self.state = 1;
         return;
       }
+      self.user = null;
       self.state = 0;
     });
   }
   getState() {
     return this.state;
+  }
+  getUser() {
+    return this.user;
   }
   signIn(username, password) {
     return this.auth.signInWithEmailAndPassword(username, password);
