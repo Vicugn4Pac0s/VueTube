f77cea44d1b36249b4428dc61278dbf8619bc478
Yoshiki Yamada <vicugnapacos.g@gmail.com>
2021-02-06 16:46:29 +0900

    エンターでログイン

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -2,7 +2,7 @@
   <div class="signin">
     <h2>Sign in</h2>
     <input type="text" placeholder="Username" v-model="username" />
-    <input type="password" placeholder="Password" v-model="password" />
+    <input type="password" placeholder="Password" v-model="password" @keydown.enter="signIn" />
     <button @click="signIn">Signin</button>
     <p>
       You don't have an account?
