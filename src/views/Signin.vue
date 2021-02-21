9379ea4fcb7a255effeadfd2aee2d1266ad9eac9
Yoshiki Yamada <vicugnapacos.g@gmail.com>
2021-02-21 17:37:57 +0900

    formデザイン調整（仮）

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="signin">
-    <h2>Sign in</h2>
-    <input type="text" placeholder="Username" v-model="username" />
-    <input
-      type="password"
-      placeholder="Password"
-      v-model="password"
-      @keydown.enter="signIn"
-    />
-    <button @click="signIn">Signin</button>
-    <p>
-      You don't have an account?
-      <router-link to="/signup">create account now!!</router-link>
-    </p>
+    <div class="formCont">
+      <div class="formCont_inner">
+        <h2 class="formCont_ttl">Sign in</h2>
+        <input type="text" placeholder="Username" v-model="username" />
+        <input
+          type="password"
+          placeholder="Password"
+          v-model="password"
+          @keydown.enter="signIn"
+        />
+        <button @click="signIn" class="formCont_submit">Signin</button>
+        <p>
+          You don't have an account?
+          <router-link to="/signup">create account now!!</router-link>
+        </p>
+      </div>
+    </div>
   </div>
   <WaitLayer :isWait="isWait" />
 </template>
@@ -59,32 +63,45 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
 }
 .signin {
-  margin-top: 20px;
-
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  align-items: center;
+  margin: 60px 0;
 }
-input {
-  margin: 10px 0;
+.formCont {
+  background: #f9f9f9;
+  box-sizing: border-box;
+  padding: 40px;
+  margin:  0 auto;
+  max-width: 450px;
+}
+.formCont input {
+  background: #fff;
+  display: block;
   padding: 10px;
-  width: 280px;
+  margin-bottom: 20px;
+  width: 100%;
+}
+.formCont_ttl {
+  color: #42b983;
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+.formCont_submit {
+  background: #42b983;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+  height: 50px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 1.2em;
+  margin-bottom: 10px;
+  letter-spacing: 0.05em;
+  text-align: center;
+  text-transform: uppercase;
+  transition: background 0.3s ease-in-out;
+  width: 100%;
 }
 </style>
